15f4efef3b2e563d13a296f3544332fa05638052
Phuree Limphornpugdee <phureelimphornpugdee@Phurees-MacBook-Air.local>
2024-01-27 17:05:57 +0700

    Excercise callbacks and higher order functions by Tar

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,23 @@
 
 function forEach(array, operation) {
   // Start coding here
+let newArray = [];
+for(let i=0; i< array.length; i++)
+{ 
+newArray.push(operation(array[i]))
 }
+return newArray
+};
+
+function addSalary(salaryArray) {
+  return salaryArray + 5000
+};
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+let newEmployeeSalaries = [];
 
 // Using `forEach` function here
+newEmployeeSalaries = forEach(employeeSalaries,addSalary);
+
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
